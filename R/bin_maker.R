ad94f3e6acf775a059bead7da30183cf1cec0dae
Yucheol Shin <brongersmai2@gmail.com>
2024-04-12 22:15:32 +0900

    commit

---
+++ R/bin_maker.R
@@ -0,0 +1,18 @@
+#' Convert continuous ENM predictions into binary presence/absence maps
+#'
+#' @param preds RasterStack object containing continuous ENM predictions
+#' @param th A list of numeric threshold values
+#' @return RasterStack of binary maps
+#' @examples
+#' get.binary <- bin_maker(preds = enm.preds, th = list(0.1, 0.23, 0.43))
+
+bin_maker <- function(preds, th) {
+  binary.maps <- list()
+
+  for (i in 1:nlayers(preds)) {
+    bin <- ecospat::ecospat.binary.model(Pred = terra::rast(preds[[i]]), Threshold = th[[i]]) %>% raster::raster()
+    binary.maps[[i]] <- bin
+    binary.out <- raster::stack(binary.maps)
+  }
+  return(binary.out)
+}
