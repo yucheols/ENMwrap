5ea526474b1ec9775640685173568256d4e4e832
Yucheol Shin <brongersmai2@gmail.com>
2024-04-13 12:25:38 +0900

    add code and document

---
+++ R/occs_thinner.R
@@ -0,0 +1,22 @@
+#' Spatial thinning of (multiple) species occurrence datasets, using the SDMtune package (Vignali et al. 2020)
+#'
+#' @param occs_list A list of data.frame objects for species occurrence points. Need to have "long" and "lat" columns
+#' @param envs RasterLayer. The spatial resolution of this layer will be used as a spatial thinning distance
+#' @param long Character. Name of the longitude column
+#' @param lat Character. Name of the latitude column
+#' @return A list of dataframes containing the thinned occurrence points for each input species
+#' @examples
+#' thin_occs <- occs_thinner(occs_list = occs_list, envs = envs, long = 'long', lat = 'lat')
+
+
+occs_thinner <- function(occs_list, envs, long, lat){
+  output <- list()
+
+  for (i in 1:length(occs_list)) {
+    occs <- occs_list[[i]]
+    thin <- SDMtune::thinData(coords = occs, env = terra::rast(envs), x = long, y = lat, verbose = T, progress = T)
+    output[[i]] <- thin
+  }
+  return(output)
+}
+
