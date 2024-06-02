bcb6d9d2566ea044c5ad38116cc22df2c7ebdb52
Yucheol Shin <brongersmai2@gmail.com>
2024-06-03 01:56:27 +0900

    add arg

--- R/occs_thinner.R
+++ R/occs_thinner.R
@@ -4,19 +4,22 @@
 #' @param envs RasterLayer. The spatial resolution of this layer will be used as a spatial thinning distance
 #' @param long Character. Name of the longitude column
 #' @param lat Character. Name of the latitude column
+#' @param spp_list A list of input species names
 #' @returns A list of dataframes containing the thinned occurrence points for each input species
 #' @examples
-#' thin_occs <- occs_thinner(occs_list = occs_list, envs = envs, long = 'long', lat = 'lat')
+#' thin_occs <- occs_thinner(occs_list = occs_list, envs = envs, long = 'long', lat = 'lat', spp_list = spp_list)
 
 
-occs_thinner <- function(occs_list, envs, long, lat){
+occs_thinner <- function(occs_list, envs, long, lat, spp_list){
   output <- list()
 
   for (i in 1:length(occs_list)) {
+    print(paste0('start thinning', spp_list[[i]]))
     occs <- occs_list[[i]]
     thin <- SDMtune::thinData(coords = occs, env = terra::rast(envs), x = long, y = lat, verbose = T, progress = T)
     output[[i]] <- thin
   }
   return(output)
+  print('Done!')
 }
 
