8a4106a88d3a8420463993e715947b51caffe8d8
Yucheol Shin <brongersmai2@gmail.com>
2024-08-24 10:56:37 +0900

    update model_predictr and document

--- R/model_predictr.R
+++ R/model_predictr.R
@@ -3,6 +3,7 @@
 #' @param model A fitted ENM of the class MaxEnt. Can be extracted by accessing the "models" slot output from "test_multibg" or "test_multisp"
 #' @param preds.list A list containing RasterStack objects of projection datasets
 #' @param preds.names A vector of names corresponding to each dataset in "preds.list". Can be the name of a particular time period (e.g. LGM) or climate change scenario (e.g. SSP26)
+#' @param method Specify the type of model prediction. If "single2multi", conduct model predictions to multiple past/future environment from a single model. If "multi2single", make predictions to a single past/future dataset from multiple current models
 #' @returns RasterStack of ENM predictions
 #' @examples
 #' pred.enms <- model_predictr(model = mx_model, preds.list = list(lgm, mh, current), preds.names = c('LGM', 'Mid-Holocene', 'Current'))
@@ -12,13 +13,13 @@ model_predictr <- function(model, preds.list, preds.names, method) {
 
   output <- list()
 
-  if(method = 'single2multi') {
+  if(method == 'single2multi') {
     for (i in 1:length(preds.list)) {
       make.pred <- dismo::predict(object = model, x = preds.list[[i]], progress = 'text')
       output[[i]] <- make.pred
     }
   }
-  else if(method = 'multi2single') {
+  else if(method == 'multi2single') {
     for (i in 1:length(preds.list)) {
       make.pred <- dismo::predict(object = model[[i]], x = preds.list, progress = 'text')
       output[[i]] <- make.pred
