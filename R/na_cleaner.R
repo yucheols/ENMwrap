72d545b69c53fba73ce96ea9f12e8253961af638
Yucheol Shin <brongersmai2@gmail.com>
2025-04-17 10:12:44 -0400

    improve new function

--- R/na_cleaner.R
+++ R/na_cleaner.R
@@ -9,12 +9,16 @@ na_cleaner <- function(pts, envs, x, y) {
       ext <- raster::extract(envs, pts[[i]]) %>% as.data.frame()
       ext.bind <- cbind(ext, pts[[i]]) %>% na.omit() %>% dplyr::select(x, y)
       cleaned_pts[[i]] <- ext.bind
+      print(paste0('The number of points after NA cleaning is   ', nrow(cleaned_pts[[i]])))
     }
   }
 
   if (length(pts) == 1) {
+    pts <- dplyr::bind_rows(pts)
     ext <- raster::extract(envs, pts) %>% as.data.frame()
     cleaned_pts <- cbind(ext, pts) %>% na.omit() %>% dplyr::select(x, y)
+    print(paste0('The number of points after NA cleaning is   ', nrow(cleaned_pts)))
   }
+  print('NA cleaning completed!')
   return(cleaned_pts)
 }
