ca2041ecc87aa4d873e78333d7ce780bae497098
Yucheol Shin <brongersmai2@gmail.com>
2025-04-17 10:06:15 -0400

    commit new function

--- R/na_cleaner.R
+++ R/na_cleaner.R
@@ -4,7 +4,7 @@
 na_cleaner <- function(pts, envs, x, y) {
   cleaned_pts <- list()
 
-  if (length(pts) => 2) {
+  if (length(pts) >= 2) {
     for (i in 1:length(pts)) {
       ext <- raster::extract(envs, pts[[i]]) %>% as.data.frame()
       ext.bind <- cbind(ext, pts[[i]]) %>% na.omit() %>% dplyr::select(x, y)
