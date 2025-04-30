548150f5fbb9f3bcc07fe8d0521681b10bc223a6
Yucheol Shin <brongersmai2@gmail.com>
2025-04-30 19:41:22 -0400

    update doc

--- R/test_multisp.R
+++ R/test_multisp.R
@@ -5,7 +5,7 @@
 #' @param envs RasterStack of environmental data
 #' @param bg A list containing data.frame of background points. Each data.frame in the list should have two columns with the column names matching the occurrence dataset. If using the same background dataset for all species, repeat the data.frame so that the list matches the length of occs.list (e.g. you can use bg = rep(list(bg), length(occs.list))). Alternatively, you can use different background dataset for each species.
 #' @param tune.args A named list containing feature class settings ("fc") and regularization values ("rm"). Use ?ENMevaluate for details
-#' @param partitions Character. The name of data partitioning strategy to use for model evaluations. Can select from: "randomkfold", "block", "checkerboard1", "checkerboard2", "testing", and "user"
+#' @param partitions Character. The name of data partitioning strategy to use for model evaluations. Can select from: "randomkfold", "block", "checkerboard", "testing", and "user"
 #' @param partition.settings A named list used to specify settings for the selected partitioning scheme
 #' @param user.grp A named list designating user-specified cross-validation folds. Only required when "partition.settings" is set to "user"
 #' @param type Character. Select a method for selecting the optimal parameter combinations. If "type1" is used, the optimal model is selected based on the minimum 10 percent omission rate as a primary criterion, minimum AUCDIFF value as a secondary criterion, and maximum validation AUC as a tertiary criterion.
